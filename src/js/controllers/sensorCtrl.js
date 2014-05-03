6f0c567315d27e79b2b877e06aedb49ab021b973
Harri Ohra-aho <hoek@iki.fi>
2014-05-04 00:33:01 +0300

    More admin

--- src/js/controllers/sensorCtrl.js
+++ src/js/controllers/sensorCtrl.js
@@ -1,4 +1,5 @@
 var $ = require('jquery');
+var _ = require('lodash');
 
 exports.sensorCtrl = function($scope, $http, $interval) {
 
@@ -14,8 +15,8 @@ exports.sensorCtrl = function($scope, $http, $interval) {
       });
   };
 
-  $scope.selectSensor = function(item){
-    if ($scope.isSelectedSensor(item)){
+  $scope.selectSensor = function(item) {
+    if ($scope.isSelectedSensor(item)) {
       $scope.selectedSensor = undefined;
     } else {
       $scope.selectedSensor = item;
@@ -23,32 +24,46 @@ exports.sensorCtrl = function($scope, $http, $interval) {
     $scope.selectedProbe = undefined;
   };
 
-  $scope.isSelectedSensor = function(item){
-    return $scope.selectedSensor === item;
+  $scope.isSelectedSensor = function(item) {
+    return $scope.anySensorSelected() && $scope.selectedSensor._id === item._id;
   };
     
   $scope.anySensorSelected = function() {
     return $scope.selectedSensor !== undefined;
   };
 
-  $scope.selectProbe = function(item){
-    if ($scope.isSelectedProbe(item)){
+  $scope.deleteSensor = function() {
+    var deleted = $scope.selectedSensor;
+    $scope.close();
+    $scope.sensors = _.reject($scope.sensors, function(sensor) { return deleted._id === sensor._id; });
+  };
+
+  $scope.selectProbe = function(item) {
+    if ($scope.isSelectedProbe(item)) {
       $scope.selectedProbe = undefined;
     } else {
-      $scope.selectedProbe = item;
+      $scope.selectedProbe = angular.copy(item);
     }
   };
 
-  $scope.isSelectedProbe = function(item){
+  $scope.isSelectedProbe = function(item) {
     return $scope.selectedProbe === item;
   };
     
   $scope.anyProbeSelected = function() {
     return $scope.selectedProbe !== undefined;
   };
+
+  $scope.saveProbe = function() {
+    var probe = _.find($scope.selectedSensor.probes, function(item) { return item.id === $scope.selectedProbe.id; });
+    probe.name = $scope.selectedProbe.name;
+    probe.id = $scope.selectedProbe.id;
+    probe.humidity = $scope.selectedProbe.humidity;
+  };
     
   $scope.close = function() {
     $scope.selectedSensor = undefined;
+    $scope.selectedProbe = undefined;
   };
 
   $scope.fetchSensors();
